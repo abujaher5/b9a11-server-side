b9096f48868b07e552edfbf558ca459fa1987061
abujaher5 <abujaher5120@gmail.com>
2024-06-01 09:55:37 +0600

    added basic setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  credentials: "true",
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello ");
+});
+
+app.listen(port, () => console.log(`server running on the port ${port}`));
